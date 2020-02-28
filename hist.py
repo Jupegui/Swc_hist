4d7b831d98618d41237110312d7c01bebc912a12
Jonathan Upegui <upegui@Jonathans-MacBook-Air.local>
2020-02-28 10:40:22 -0500

    final script? too man claims?

--- hist.py
+++ hist.py
@@ -1,6 +1,7 @@
 # randomizing data, generating summary statistics, and histogram
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 mu = 80
 sigma = 10
@@ -10,3 +11,9 @@ x = np.random.normal(mu, sigma, 100)
 print("random normal array mean centered", x[:10])
 
 print("mean", np.mean(x))
+
+plt.hist(x)
+plt.show
+
+print("I am a champion of science!")
+
